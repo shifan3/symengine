0ed4c71bb757bc1b96b11127cf2bd9b2384829bc
shi.fan <shi.fan@danatech.com>
2020-08-31 09:28:55 +0800

    dsfsfs

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
+mkdir -p build
 cd build
+#cmake .. -G"Unix Makefiles" -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no -DINTEGER_CLASS=boostmp -DCMAKE_INSTALL_PREFIX=../libs
 make && make install && \
     cp ../libs/lib/libsymengine.a ../../calclib/libs/libsymengine.a && \
     cp ../libs/include/symengine ../../calclib/include/ -rf 
