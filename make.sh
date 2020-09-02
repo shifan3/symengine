4bc51ca367fe9ed94fa70641ec23d8b167c554b8
shi.fan <shi.fan@danatech.com>
2020-09-02 16:22:42 +0800

    asdad

--- make.sh
+++ make.sh
@@ -1,7 +1,6 @@
 mkdir -p build
 cd build
-#cmake .. -G"Unix Makefiles" -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no -DINTEGER_CLASS=boostmp -DCMAKE_INSTALL_PREFIX=../libs
-make && make install && \
-    cp ../libs/lib/libsymengine.a ../../calclib/libs/libsymengine.a && \
-    cp ../libs/include/symengine ../../calclib/include/ -rf 
+cmake .. -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no -DINTEGER_CLASS=boostmp -DCMAKE_INSTALL_PREFIX=../libs -DBoost_INCLUDE_DIR=../boost_1_74_0
+make 
+make install
 cd ..
