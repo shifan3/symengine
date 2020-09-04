57b0f241bcadf92646df66eddba0a1e3bf64f9b6
shi.fan <shi.fan@danatech.com>
2020-09-04 14:18:26 +0800

    build script

--- make.sh
+++ make.sh
@@ -1,6 +1,10 @@
+rm libs -rf
 mkdir -p build
 cd build
-cmake .. -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no -DINTEGER_CLASS=boostmp -DCMAKE_INSTALL_PREFIX=../libs -DBoost_INCLUDE_DIR=../boost_1_74_0
+#cmake .. -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no -DINTEGER_CLASS=boostmp -DCMAKE_INSTALL_PREFIX=../libs -DBoost_INCLUDE_DIR=../boost_1_74_0
 make 
 make install
 cd ..
+if [ ! -f libs/lib/libsymengine.a ]; then
+    cp libs/lib64/libsymengine.a libs/lib/
+fi
