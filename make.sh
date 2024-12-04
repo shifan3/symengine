4c6c3ee099b1a6eaafa6daf07dfffb103f4608df
shifan3rd <shifan3rd@gmail.com>
2024-12-04 07:34:23 +0000

    make faster

--- make.sh
+++ make.sh
@@ -1,6 +1,9 @@
 rm libs -rf
 mkdir -p build
 cd build
-cmake .. -DWITH_FLINT=ON -DWITH_MPFR:BOOL=ON 
-make -j 8
-sudo make install
+cmake .. -DWITH_FLINT=ON -DWITH_MPFR:BOOL=ON -DBUILD_TESTS=OFF
+make -j 16
+if [ "$(id -u)" -ne 0 ]; then
+    sudo make install
+fi
+make install
