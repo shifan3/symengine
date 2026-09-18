6160fbca68a2f71cf3f8f2e6df3170c67c393d75
shifan3rd <shifan3rd@gmail.com>
2026-09-18 06:14:22 +0000

    Disable benchmarks in build process and improve fmpq_poly_wrapper initialization with fmpz and fmpq types.

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 rm libs -rf
 mkdir -p build
 cd build
-cmake .. -DWITH_FLINT=ON -DWITH_MPFR:BOOL=ON -DBUILD_TESTS=OFF
+cmake .. -DWITH_FLINT=ON -DWITH_MPFR:BOOL=ON -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF
 make -j 16
 if [ "$(id -u)" -ne 0 ]; then
     sudo make install
